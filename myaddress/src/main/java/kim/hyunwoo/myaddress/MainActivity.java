0f2b1b2a5430c46119080808e9fb7c84d99ac812
heptacode <admin@hyunwoo.kim>
2019-12-04 10:17:48 +0900

    Create Module MyAddress

---
+++ myaddress/src/main/java/kim/hyunwoo/myaddress/MainActivity.java
@@ -0,0 +1,61 @@
+package kim.hyunwoo.myaddress;
+
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button contactBtn;
+    TextView nameView;
+    TextView phoneView;
+    TextView emailView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactBtn=findViewById(R.id.lab2_btn);
+        nameView=findViewById(R.id.name);
+        phoneView=findViewById(R.id.phone);
+        emailView=findViewById(R.id.email);
+
+        contactBtn.setOnClickListener(this);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 100);
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==contactBtn){
+           // 주소록 앱에 인텐트 보내기
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String s = data.getDataString();
+        nameView.setText(s);
+    }
+}
+
